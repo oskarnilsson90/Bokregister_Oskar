9e8e29fb910b11cc735d6e14535c534efb9907a2
Oskar Nilsson <143490244+oskarnilsson90@users.noreply.github.com>
2024-09-06 15:13:25 +0200

    Styling and fixing

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// // Immediately-invoked function expression (IIFE) to fetch movies and reviews
-// (async () => {
-//     // Fetch movies from the API
-//     const books = await API.getBooks();
-
-//     console.log(books);
-
-//     // Sort movies alphabetically by title
-//     books.sort((a, b) => a.author.localeCompare(b.author));
-
-//     // Get the movies container element
-//     const booksDiv = document.getElementById("books");
-
-//     // Display each movie in the container
-//     for (const book of books) {
-//         const bookDiv = document.createElement("tbody");
-
-//         // Apply class to movie container
-//         bookDiv.className = "table";
-
-//         bookDiv.innerHTML = `
-//             <tbody id="books">
-//                 <td>${book.title}</td>
-//                 <td>${book.author}</td>
-//                 <td>${book.isbn}</td>
-//                 <td> <a href="book.html?id=${book.id}">Show Book<a/> </td>
-//             </tbody>
-//             `;
-
-//         booksDiv.appendChild(bookDiv);
-
-//     }
-
-// })();
-
 // Immediately-invoked function expression (IIFE) to fetch books and reviews
 (async () => {
     // Fetch books from the API
